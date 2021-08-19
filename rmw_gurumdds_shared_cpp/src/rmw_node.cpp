31e84074f634c562a86db55474ebce9a73eaa499
Youngjin Yun <youngjin@gurum.cc>
2021-08-19 14:41:30 +0900

    Imported upstream version '3.0.3' of 'upstream'

--- rmw_gurumdds_shared_cpp/src/rmw_node.cpp
+++ rmw_gurumdds_shared_cpp/src/rmw_node.cpp
@@ -41,9 +41,7 @@ shared__rmw_create_node(
   const char * implementation_identifier,
   rmw_context_t * context,
   const char * name,
-  const char * namespace_,
-  size_t domain_id,
-  bool localhost_only)
+  const char * namespace_)
 {
   RCUTILS_CHECK_ARGUMENT_FOR_NULL(context, NULL);
   RMW_CHECK_TYPE_IDENTIFIERS_MATCH(
@@ -104,7 +102,8 @@ shared__rmw_create_node(
 
   // TODO(clemjh): Implement security features
 
-  if (localhost_only) {
+  dds_DomainId_t domain_id = static_cast<dds_DomainId_t>(context->actual_domain_id);
+  if (context->options.localhost_only == RMW_LOCALHOST_ONLY_ENABLED) {
     dds_StringProperty props[] = {
       {const_cast<char *>("rtps.interface.ip"),
         const_cast<void *>(static_cast<const void *>("127.0.0.1"))},
