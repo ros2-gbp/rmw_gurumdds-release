9e24ee9f6d61a7a86366958e2f8bec115ba1767c
Youngjin Yun <youngjin@gurum.cc>
2022-11-07 15:51:25 +0900

    Rebase from 'rpm/rolling/rmw_gurumdds_cpp'

--- src/rmw_features.cpp
+++ src/rmw_features.cpp
@@ -1,4 +1,4 @@
-// Copyright 2019 GurumNetworks, Inc.
+// Copyright 2022 GurumNetworks, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "rmw_gurumdds_cpp/identifier.hpp"
+#include "rmw/features.h"
 
-const char * const RMW_GURUMDDS_ID = "rmw_gurumdds_cpp";
+bool
+rmw_feature_supported(rmw_feature_t feature)
+{
+  return feature == RMW_FEATURE_MESSAGE_INFO_PUBLICATION_SEQUENCE_NUMBER ? true : false;
+}
--- src/rmw_topic_names_and_types.cpp
+++ src/rmw_topic_names_and_types.cpp
@@ -17,14 +17,22 @@
 #include <string>
 
 #include "rcutils/allocator.h"
+#include "rcutils/logging_macros.h"
+#include "rcutils/strdup.h"
+#include "rcutils/types/string_array.h"
 
 #include "rmw/allocators.h"
+#include "rmw/convert_rcutils_ret_to_rmw_ret.h"
+#include "rmw/error_handling.h"
 #include "rmw/get_topic_names_and_types.h"
+#include "rmw/impl/cpp/macros.hpp"
 #include "rmw/rmw.h"
 
-#include "rmw_gurumdds_shared_cpp/rmw_common.hpp"
-
+#include "rmw_gurumdds_cpp/dds_include.hpp"
+#include "rmw_gurumdds_cpp/demangle.hpp"
 #include "rmw_gurumdds_cpp/identifier.hpp"
+#include "rmw_gurumdds_cpp/names_and_types_helpers.hpp"
+#include "rmw_gurumdds_cpp/rmw_context_impl.hpp"
 
 extern "C"
 {
@@ -35,7 +43,29 @@ rmw_get_topic_names_and_types(
   bool no_demangle,
   rmw_names_and_types_t * topic_names_and_types)
 {
-  return shared__rmw_get_topic_names_and_types(
-    gurum_gurumdds_identifier, node, allocator, no_demangle, topic_names_and_types);
+  RMW_CHECK_ARGUMENT_FOR_NULL(node, RMW_RET_INVALID_ARGUMENT);
+  RCUTILS_CHECK_ALLOCATOR_WITH_MSG(
+    allocator, "allocator argument is invalid", return RMW_RET_INVALID_ARGUMENT);
+  RMW_CHECK_TYPE_IDENTIFIERS_MATCH(
+    node,
+    node->implementation_identifier,
+    RMW_GURUMDDS_ID,
+    return RMW_RET_INCORRECT_RMW_IMPLEMENTATION);
+
+  if (rmw_names_and_types_check_zero(topic_names_and_types) != RMW_RET_OK) {
+    return RMW_RET_INVALID_ARGUMENT;
+  }
+
+  DemangleFunction demangle_topic = _demangle_ros_topic_from_topic;
+  DemangleFunction demangle_type = _demangle_if_ros_type;
+
+  if (no_demangle) {
+    demangle_topic = _identity_demangle;
+    demangle_type = _identity_demangle;
+  }
+
+  auto common_ctx = &node->context->impl->common_ctx;
+  return common_ctx->graph_cache.get_names_and_types(
+    demangle_topic, demangle_type, allocator, topic_names_and_types);
 }
 }  // extern "C"
