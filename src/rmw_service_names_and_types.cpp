b7d40fe849069cd5cf530be19b1ed5d8b6340346
Youngjin Yun <youngjin@gurum.cc>
2022-11-07 15:51:24 +0900

    Rebase from 'release/rolling/rmw_gurumdds_cpp'

--- src/rmw_service_names_and_types.cpp
+++ src/rmw_service_names_and_types.cpp
@@ -12,16 +12,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <map>
+#include <set>
+#include <string>
+
+#include "rcutils/allocator.h"
+#include "rcutils/logging_macros.h"
+#include "rcutils/strdup.h"
+#include "rcutils/types/string_array.h"
+
 #include "rmw/allocators.h"
 #include "rmw/convert_rcutils_ret_to_rmw_ret.h"
 #include "rmw/error_handling.h"
 #include "rmw/get_service_names_and_types.h"
+#include "rmw/impl/cpp/macros.hpp"
 #include "rmw/names_and_types.h"
 #include "rmw/rmw.h"
 
-#include "rmw_gurumdds_shared_cpp/rmw_common.hpp"
-
+#include "rmw_gurumdds_cpp/dds_include.hpp"
+#include "rmw_gurumdds_cpp/demangle.hpp"
 #include "rmw_gurumdds_cpp/identifier.hpp"
+#include "rmw_gurumdds_cpp/names_and_types_helpers.hpp"
+#include "rmw_gurumdds_cpp/rmw_context_impl.hpp"
 
 extern "C"
 {
@@ -31,7 +43,24 @@ rmw_get_service_names_and_types(
   rcutils_allocator_t * allocator,
   rmw_names_and_types_t * service_names_and_types)
 {
-  return shared__rmw_get_service_names_and_types(
-    gurum_gurumdds_identifier, node, allocator, service_names_and_types);
+  RMW_CHECK_ARGUMENT_FOR_NULL(node, RMW_RET_INVALID_ARGUMENT);
+  RCUTILS_CHECK_ALLOCATOR_WITH_MSG(
+    allocator, "allocator argument is invalid", return RMW_RET_INVALID_ARGUMENT);
+  RMW_CHECK_TYPE_IDENTIFIERS_MATCH(
+    node,
+    node->implementation_identifier,
+    RMW_GURUMDDS_ID,
+    return RMW_RET_INCORRECT_RMW_IMPLEMENTATION);
+
+  if (rmw_names_and_types_check_zero(service_names_and_types) != RMW_RET_OK) {
+    return RMW_RET_INVALID_ARGUMENT;
+  }
+
+  auto common_ctx = &node->context->impl->common_ctx;
+  return common_ctx->graph_cache.get_names_and_types(
+    _demangle_service_from_topic,
+    _demangle_service_type_only,
+    allocator,
+    service_names_and_types);
 }
 }  // extern "C"
