1293bf71fa84adf9ff2d4abb9f8a70165203b03e
kumazuma <seontai@gurum.cc>
2025-12-03 17:04:48 +0900

    Revert "Trimmed the branch to only the gurumdds_cmake_module sub directory"
    This reverts commit 67e8d96696b01d2dfb3072fb8d846938b73662b9.

---
+++ rmw_gurumdds_cpp/include/rmw_gurumdds_cpp/event_converter.hpp
@@ -0,0 +1,49 @@
+// Copyright 2019 GurumNetworks, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef RMW_GURUMDDS__EVENT_CONVERTER_HPP_
+#define RMW_GURUMDDS__EVENT_CONVERTER_HPP_
+
+#include "rmw/event.h"
+#include "rmw/ret_types.h"
+#include "rmw/types.h"
+
+#include "rmw_gurumdds_cpp/dds_include.hpp"
+
+namespace rmw_gurumdds_cpp
+{
+/// Return the corresponding DDS_StatusKind to the input RMW_EVENT.
+/**
+ * \param event_t to translate to status kind
+ * \return the StatusKind corresponding to the rmw_event_type_t
+ */
+dds_StatusKind get_status_kind_from_rmw(rmw_event_type_t event_t);
+
+/// Return true if the input RMW event has a corresponding DDS_StatusKind.
+/**
+ * \param event_t input rmw event to check
+ * \return true if there is an RMW to DDS_StatusKind mapping, false otherwise
+ */
+bool is_event_supported(rmw_event_type_t event_t);
+
+/// Assign the input DDS return code to its corresponding RMW return code.
+/**
+  * \param dds_return_code input DDS return code
+  * \return to_return the corresponding rmw_ret_t that maps to the input DDS_ReturnCode_t. By
+  * default RMW_RET_ERROR is returned if no corresponding rmw_ret_t is not defined.
+  */
+rmw_ret_t check_dds_ret_code(dds_ReturnCode_t dds_return_code);
+} // namespace rmw_gurumdds_cpp
+
+#endif // RMW_GURUMDDS__EVENT_CONVERTER_HPP_
---
+++ rmw_gurumdds_cpp/include/rmw_gurumdds_cpp/event_info_service.hpp
@@ -0,0 +1,79 @@
+// Copyright 2019 GurumNetworks, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef RMW_GURUMDDS__EVENT_INFO_SERVICE_HPP_
+#define RMW_GURUMDDS__EVENT_INFO_SERVICE_HPP_
+
+#include "rmw/event_callback_type.h"
+#include "rmw/types.h"
+
+#include "rosidl_runtime_c/message_type_support_struct.h"
+#include "rosidl_runtime_c/service_type_support_struct.h"
+
+#include "rmw_gurumdds_cpp/dds_include.hpp"
+#include "rmw_gurumdds_cpp/event_info_common.hpp"
+
+namespace rmw_gurumdds_cpp
+{
+struct ClientInfo
+{
+  const rosidl_service_type_support_t * service_typesupport;
+  const char * implementation_identifier;
+  rmw_context_impl_t * ctx;
+  int64_t sequence_number;
+  uint8_t writer_guid[16];
+
+  rmw_gid_t publisher_gid;
+  rmw_gid_t subscriber_gid;
+  dds_DataWriter * request_writer;
+  dds_DataReader * response_reader;
+  dds_ReadCondition * read_condition;
+
+  dds_DataReaderListener response_listener;
+  dds_DataSeq * data_seq;
+  dds_SampleInfoSeq * info_seq;
+  dds_UnsignedLongSeq * raw_data_sizes;
+  event_callback_data_t event_callback_data;
+
+  size_t count_unread()
+  {
+    return rmw_gurumdds_cpp::count_unread(response_reader, data_seq, info_seq, raw_data_sizes);
+  }
+};
+
+struct ServiceInfo {
+  const rosidl_service_type_support_t * service_typesupport;
+  const char * implementation_identifier;
+  rmw_context_impl_t * ctx;
+
+  rmw_gid_t publisher_gid;
+  rmw_gid_t subscriber_gid;
+  dds_DataWriter * response_writer;
+  dds_DataReader * request_reader;
+  dds_ReadCondition * read_condition;
+
+  dds_DataReaderListener request_listener;
+  dds_DataSeq * data_seq;
+  dds_SampleInfoSeq * info_seq;
+  dds_UnsignedLongSeq * raw_data_sizes;
+  event_callback_data_t event_callback_data;
+
+  size_t count_unread()
+  {
+    return rmw_gurumdds_cpp::count_unread(request_reader, data_seq, info_seq, raw_data_sizes);
+  }
+};
+} // namespace rmw_gurumdds_cpp
+
+#endif // RMW_GURUMDDS__EVENT_INFO_SERVICE_HPP_
